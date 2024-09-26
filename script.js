3f8716dcd5a59ef27f856d0dd7df34addfde0d09
ThomasMaizonave <139795025+ThomasMaizonav@users.noreply.github.com>
2024-09-26 20:36:00 -0300

    atualizacoes

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const mario = document.querySelector('.mario')
-const pipe = document.querySelector('.pipe')
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const gameBoard = document.querySelector('#game-board');
+const menu = document.querySelector('#menu');
+const startButton = document.querySelector('#startButton');
+const restartMenu = document.querySelector('#restartMenu');
+const restartButton = document.querySelector('#restartButton');
 
 const jump = () => {
-    mario.classList.add('jump')
+    mario.classList.add('jump');
 
     setTimeout(() => {
-        mario.classList.remove('jump')
-    }, 500)
-}
+        mario.classList.remove('jump');
+    }, 500);
+};
 
-const loop = setInterval (() => {
+// Função para iniciar o jogo
+const startGame = () => {
+    menu.style.display = 'none';
+    gameBoard.style.display = 'block';
+};
 
-    console.log('loop')
-    
-    const pipePosition = pipe.offsetLeft
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+// Função para reiniciar o jogo
+const restartGame = () => {
+    location.reload(); // Recarrega a página para reiniciar o jogo
+};
 
-    console.log(marioPosition)
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
@@ -25,12 +37,20 @@ const loop = setInterval (() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = './images/game-over.png'
-        mario.style.widht = '75px'
-        mario.style.marginLeft = '50px'
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
 
-        clearInterval(loop)
+        clearInterval(loop);
+
+        // Mostra o botão de reiniciar
+        restartMenu.style.display = 'block';
+    
     }
-}, 10)
+}, 10);
 
-document.addEventListener('keydown', jump)
+document.addEventListener('keydown', jump);
+
+// Eventos de clique para iniciar e reiniciar o jogo
+startButton.addEventListener('click', startGame);
+restartButton.addEventListener('click', restartGame);
